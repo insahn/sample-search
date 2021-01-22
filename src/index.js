f1d1572bd40060c26dd1dc62117ee090d1e073f6
Valentin Bahchevanov <valentin.bahchevanov@scalefocus.com>
2021-01-22 14:11:05 +0200

    remove apollo from exporting.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,77 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-import React from "react";
-import App from "./components/App";
+import React, { useState } from "react";
+import { gql, useQuery } from "@apollo/client";
 
-const client = new ApolloClient({
-  uri: "https://graphql.anilist.co",
-  cache: new InMemoryCache({
-    addTypename: false,
-  }),
-});
+const GET_USERS = gql`
+  query($search: String, $sort: [UserSort], $perPage: Int, $page: Int) {
+    Page(perPage: $perPage, page: $page) {
+      users(search: $search, sort: $sort) {
+        id
+        name
+      }
+      pageInfo {
+        total
+        currentPage
+        lastPage
+        hasNextPage
+        perPage
+      }
+    }
+  }
+`;
 
-export const SampleSearch = () => {
+const SampleSearch = () => {
+  const [page, setCurrentPage] = useState(1);
+  const [search, setSearch] = useState("");
+
+  const nextPage = React.useCallback(() => {
+    setCurrentPage((p) => p + 1);
+  }, []);
+
+  const prevPage = React.useCallback(() => {
+    setCurrentPage((p) => p - 1);
+  }, []);
+
+  const handleChange = React.useCallback((e) => setSearch(e.target.value), []);
+
+  const { loading, data } = useQuery(GET_USERS, {
+    variables: {
+      search,
+      sort: "USERNAME_DESC",
+      page,
+      perPage: 20,
+    },
+  });
+
   return (
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <React.Fragment>
+      <div style={{ marginBottom: "20px" }}>
+        <span style={{ paddingRight: "10px" }}>Search user</span>
+        <input type="text" value={search} onChange={handleChange} />
+        <button onClick={() => prevPage()} disabled={page <= 1 || loading}>
+          prev
+        </button>
+        <button
+          onClick={() => nextPage()}
+          disabled={!data?.Page?.pageInfo.hasNextPage || loading}
+        >
+          next
+        </button>
+      </div>
+      {loading ? (
+        "Loading..."
+      ) : (
+        <div>
+          {data?.Page?.users.map((user) => {
+            return (
+              <div key={user.id}>
+                {user.id}: {user.name}
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </React.Fragment>
   );
 };
+
+export default SampleSearch;
