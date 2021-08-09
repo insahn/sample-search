8b30718218fad073e94614ab1eacc271d0e7e2c3
insahn <bahchevanov1991@gmail.com>
2021-08-09 13:20:21 +0300

    update package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  devtool: 'source-map',
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "source-map",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    libraryTarget: 'umd',
-    library: 'SampleSearch',
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    libraryTarget: "umd",
+    library: "SampleSearch",
   },
   resolve: {
-    extensions: ['.js', '.jsx', 'json'],
+    extensions: [".js", ".jsx", "json"],
   },
   module: {
     rules: [
@@ -19,17 +19,17 @@ module.exports = {
         test: /\.js?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-react', '@babel/preset-env'],
+            presets: ["@babel/preset-react", "@babel/preset-env"],
           },
         },
       },
     ],
   },
-  externals: {
-    react: 'react',
-    'react-dom': 'react-dom',
-    '@apollo/client': '@apollo/client',
-  },
-}
+  externals: [
+    { react: "react" },
+    { "react-dom": "react-dom" },
+    { "@apollo/client": "@apollo/client" },
+  ],
+};
